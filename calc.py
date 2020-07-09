c0fd41349de476aa8cc895d6e434d7f0c87441df
yomi.olutimehin <yomi.olutimehin@yahoo.com>
2020-07-09 13:50:05 +0100

    Adding 1st commit to remote

---
+++ calc.py
@@ -0,0 +1,9 @@
+with open("test.txt", 'r') as f:
+    text_string = f.read()
+    print(text_string)
+
+ 
+
+# f = open("test.txt", mode='w')
+# f.write("my first file\n")
+# f.close()
---
+++ calculator.py
@@ -0,0 +1,23 @@
+
+
+value_1 = input('Enter a symbol:\n')
+value_2 = input('Enter a number\n')
+value_3 = input('Enter a number\n')
+
+if value_1 == '+': 
+    output = int(value_2) + int(value_3)
+    print(output)
+elif value_1 == '-':
+    # output = int(value_2) + int(value_3)
+    print(int(value_2) - int(value_3))
+elif value_1 == '/':
+    print (int(value_2) / int(value_3))
+else: value_1 == '%'
+print (int(value_2) % int(value_3))
+
+
+
+ 
+
+
+
